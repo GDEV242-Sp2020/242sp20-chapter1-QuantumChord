1191996a2f3561d7cffd629adcd966acbee02576
QuantumChord <60354100+QuantumChord@users.noreply.github.com>
2020-02-03 19:38:57 -0500

    Update Picture.java

--- Picture.java
+++ Picture.java
@@ -1,20 +1,18 @@
 /**
- * This class represents a simple picture. You can draw the picture using
- * the draw method. But wait, there's more: being an electronic picture, it
- * can be changed. You can set it to black-and-white display and back to
- * colors (only after it's been drawn, of course).
- *
- * This class was written as an early example for teaching Java with BlueJ.
- * 
- * @author  Michael Kšlling and David J. Barnes
- * @version 2016.02.29
+ * @Author Joseph McGuane
+ * @version 2020.02.2
+ * Picture is meant to represent two people on a hill during the day. One has
+ * a hat on and the other doesn't. The sun is up but you can see the moon.
  */
 public class Picture
 {
-    private Square wall;
-    private Square window;
-    private Triangle roof;
+    private Square sky;
+    private Square hat;
+    private Triangle hill;
     private Circle sun;
+    private Circle moon;
+    private Person person1;
+    private Person person2;
     private boolean drawn;
 
     /**
@@ -22,10 +20,13 @@ public class Picture
      */
     public Picture()
     {
-        wall = new Square();
-        window = new Square();
-        roof = new Triangle();  
+        sky = new Square();
+        hat = new Square();
+        hill = new Triangle();  
         sun = new Circle();
+        moon = new Circle();
+        person1 = new Person();
+        person2 = new Person();
         drawn = false;
     }
 
@@ -35,51 +36,45 @@ public class Picture
     public void draw()
     {
         if(!drawn) {
-            wall.moveHorizontal(-140);
-            wall.moveVertical(20);
-            wall.changeSize(120);
-            wall.changeColor("blue");
-            wall.makeVisible();
-            
-            window.changeColor("black");
-            window.moveHorizontal(-120);
-            window.moveVertical(40);
-            window.changeSize(40);
-            window.makeVisible();
+            sky.moveHorizontal(-350);
+            sky.moveVertical(-250);
+            sky.changeSize(600);
+            sky.changeColor("blue");
+            sky.makeVisible();
     
-            roof.changeSize(60, 180);
-            roof.moveHorizontal(20);
-            roof.moveVertical(-60);
-            roof.makeVisible();
+            hill.changeSize(170, 450);
+            hill.moveHorizontal(40);
+            hill.moveVertical(0);
+            hill.makeVisible();
     
             sun.changeColor("yellow");
             sun.moveHorizontal(100);
             sun.moveVertical(-40);
             sun.changeSize(80);
             sun.makeVisible();
+            
+            moon.changeColor("white");
+            moon.moveHorizontal(20);
+            moon.moveVertical(-40);
+            moon.changeSize(20);
+            moon.makeVisible();
+            
+            person1.changeColor("black");
+            person1.moveHorizontal(0);
+            person1.moveVertical(40);
+            person1.makeVisible();
+            
+            hat.changeColor("red");
+            hat.moveHorizontal(-90);
+            hat.moveVertical(47);
+            hat.changeSize(20);
+            hat.makeVisible();
+            
+            person2.changeColor("black");
+            person2.moveHorizontal(-50);
+            person2.moveVertical(15);
+            person2.makeVisible();
             drawn = true;
         }
-    }
-
-    /**
-     * Change this picture to black/white display
-     */
-    public void setBlackAndWhite()
-    {
-        wall.changeColor("black");
-        window.changeColor("white");
-        roof.changeColor("black");
-        sun.changeColor("black");
-    }
-
-    /**
-     * Change this picture to use color display
-     */
-    public void setColor()
-    {
-        wall.changeColor("red");
-        window.changeColor("black");
-        roof.changeColor("green");
-        sun.changeColor("yellow");
     }
 }
